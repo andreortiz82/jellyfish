79abb5d7adbdfda2ad1606e9c4ed87dfe77832cb
Andre Ortiz <hello@andreortiz.com>
2018-07-16 16:38:05 -0500

    working card component

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Started a new project:
 `lein figwheel new jellyfish`
+
+From the `resources/public/css` folder, run: `sass --watch style.scss:style.css`
--- src/jellyfish/core.cljs
+++ src/jellyfish/core.cljs
@@ -3,12 +3,26 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "How many likes: " :likes 0 :is-open? false}))
+(defonce app-state (atom {:text "Bordered type just draws a line between content and actions.: " :likes 0 :is-open? false}))
+
+(defn card [data]
+  (sab/html [:div.card
+              [:div.card-wrapper
+                [:div {:class ["card-header", (data :color)]}
+                  [:label (data :title)]]
+
+                [:div.card-body
+                  [:p (data :text)]
+                  [:a {:href (data :url)} "Learn more"]]]]))
 
 (defn app [data]
   (sab/html
-    [:div
-     "HELLO"]))
+    [:div.cards-container
+      (card {:color "red" :title "Red" :url "http://google.com" :text "Bordered type just draws a line between content and actions."})
+      (card {:color "orange" :title "Orange" :url "http://google.com" :text "Bordered type just draws a line between content and actions."})
+      (card {:color "yellow" :title "Yellow" :url "http://google.com" :text "Bordered type just draws a line between content and actions."})
+      (card {:color "green" :title "Green" :url "http://google.com" :text "Bordered type just draws a line between content and actions."})]))
+
 
 (defn render! []
   (.render js/ReactDOM
